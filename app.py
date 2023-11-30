9a6a7b7607f82b2bae9c3c9b715886d6a08cc5ce
Oluwatobi Akoms <akomolafevicto@gmail.com>
2023-11-30 17:21:24 +0100

    logic in authenticate.py for listing devices

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, session, render_template
+from views.authenticate import login_bp, logout_bp
+from touch import myKey #this is unique to my code to import my flask app key
+from utils import get_devices
+
+
+app = Flask(__name__)
+app.secret_key = myKey
+
+app.register_blueprint(login_bp)
+app.register_blueprint(logout_bp)
+
+@app.route('/')
+def index():
+    if 'traccar_session_cookie' in session:
+        devices_data = get_devices(session['traccar_session_cookie'])
+        user_name = session.get('user_name')
+        user_id = session.get('user_id')
+        if devices_data:
+            return render_template('dashboard.html', devices_data=devices_data, user_name=user_name, user_id=user_id )
+        else:
+            return render_template('dashboard.html', error='Error fetching devices')
+        
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ views/authenticate.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, request, redirect, url_for, session, make_response
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+login_bp = Blueprint('login', __name__)
+logout_bp = Blueprint('logout', __name__)
+
+
+@login_bp.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        # get data from the login form
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Authenticate with Traccar API
+        traccar_api_url = 'https://track.roie.com.ng/api/session'
+        traccar_payload = {'email': username, 'password': password}
+        traccar_response = requests.post(traccar_api_url, data=traccar_payload, auth=HTTPBasicAuth(username, password))
+
+        # Check if uthentication was successfull. 
+        if traccar_response.status_code == 200:
+            #get and save Traccar session to allow persistent authentication as traccar_session_cookie
+            session_cookie = traccar_response.cookies.get('JSESSIONID')
+            session['traccar_session_cookie'] = session_cookie
+
+            #extract name and id of the content from the session api
+            traccar_data = traccar_response.json()
+            session['user_name'] = traccar_data.get('name')
+            session['user_id'] = traccar_data.get('id')
+
+
+            # print(traccar_data) #this is for debug
+            return redirect(url_for('index'))
+        else:
+            return render_template('index.html', error='Invalid Credentials')
+    return render_template('index.html')
+
+
+
+
+
+@logout_bp.route('/logout')
+def logout():
+    # loops through each sessions stored in flask and pop them all out. 
+    for x in list(session.keys()):
+        session.pop(x, None)
+    return redirect(url_for('index'))
