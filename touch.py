fbd0be0dd4f6abbb051c713285adf0283c7e3368
Oluwatobi Akoms <akomolafevicto@gmail.com>
2023-12-05 16:11:18 +0100

    additional functions to support app

---
+++ touch.py
@@ -0,0 +1,55 @@
+import secrets, string
+# from flask import session
+from datetime import datetime, timedelta, timezone
+
+secret_key = secrets.token_hex(16)
+myKey = '75392fe36ed27a058c950b427bb9a60e'
+roie_endpoint = 'https://track.roie.com.ng/api/'
+
+
+def getOneYearDate():
+    # Get the current date and time
+    current_datetime = datetime.now(timezone.utc)
+
+    # Add one year to the current date and time
+    one_year_later = current_datetime + timedelta(days=365)
+
+    # Convert to ISO 8601 format
+    convert_one_year_to_iso = one_year_later.isoformat()
+
+    return convert_one_year_to_iso
+
+
+# Function to generate 
+def randomPassword(length):
+    # set a variable to include alphanumerc characters
+    characters = string.ascii_letters + string.digits
+
+    # use secrets to randomize selection of all the variable to a length as passed from the function.
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    
+    print(password)
+    
+    return password
+
+
+
+def notification_wizard(type):
+    basic_notification = ['ignitionOn', 'ignitionOff', 'geoefenceEnter', 'geofenceExit']
+    if type == basic_notification:
+        data = {
+            "always":'true',
+            "type":type,
+            "notificators":"traccar,firebase,web"
+        }
+        return data
+
+    type == 'alarm'
+    data = {
+        "attributes": {"alarms":"powerCut"},
+	    "always":'true',
+	    "type":"alarm",
+	    "notificators":"traccar,firebase,web,mail"
+    }
+    return  data
+    
