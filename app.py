20bd1e0fec1f55a43a5af7f5878b7d8a57b5aef7
Oluwatobi Akoms <akomolafevicto@gmail.com>
2024-01-02 15:46:35 +0100

    registered check_user and link_user routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from touch import myKey as myKey #this is unique to my code to import my flask app key
 from flask import Flask, session, render_template
 from views.authenticate import login_bp, logout_bp
-from views.functions import adddevice_bp, reg_user_bp
+from views.functions import adddevice_bp, reg_user_bp, check_user_bp, link_user_bp
 from views.utils import get_devices
 
 
@@ -12,6 +12,8 @@ app.register_blueprint(login_bp)
 app.register_blueprint(logout_bp)
 app.register_blueprint(adddevice_bp)
 app.register_blueprint(reg_user_bp)
+app.register_blueprint(check_user_bp)
+app.register_blueprint(link_user_bp)
 
 
 @app.route('/')
