5cd3048729afe728f079ba830e70b29a542906a2
Oluwatobi Akoms <akomolafevicto@gmail.com>
2024-01-30 14:43:43 +0100

    added conditional statement to render the necesary app based on user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from touch import myKey as myKey #this is unique to my code to import my flask app key
+from touch import myKey as myKey, userRestriction #this is unique to my code to import my flask app key
 from flask import Flask, session, render_template
 from views.authenticate import login_bp, logout_bp
 from views.functions import adddevice_bp, reg_user_bp, check_user_bp, link_user_bp
@@ -24,10 +24,20 @@ def index():
         devices_data = get_devices(session['traccar_session_cookie'])
         user_name = session.get('user_name')
         user_id = session.get('user_id')
-        if devices_data:
-            return render_template('dashboard.html', devices_data=devices_data, user_name=user_name, user_id=user_id )
-        else:
-            return render_template('dashboard.html', error='Error fetching devices')
+        status = userRestriction(user_id)
+
+        if status == 'unauthorized':
+            session.clear()
+            return render_template('index.html', message="You are not authorized to use this feature")
+        
+        elif status == 'premium':
+            return render_template('premiumdb.html')
+        
+        elif status == 'admin':   
+            if devices_data:
+                return render_template('dashboard.html', devices_data=devices_data, user_name=user_name, user_id=user_id )
+            else:
+                return render_template('dashboard.html', error='Error fetching devices')
         
     return render_template('index.html')
 
